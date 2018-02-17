8dd0ddf1ba8dfa71aac2ed9a1cb59e5be2bcd934
zdenek@me.com <zdenek@me.com>
2018-02-17 16:41:03 +0100

    Prepare for handling remote references

--- packages/cli/src/commands/validateSchema.js
+++ packages/cli/src/commands/validateSchema.js
@@ -1,20 +1,36 @@
 const Ajv = require('ajv')
 const yaml = require('../lib/readYAML')
 
-const ajv = new Ajv({ allErrors: true, verbose: true, missingRefs: true })
+const ajv = new Ajv({ allErrors: true, verbose: true, missingRefs: true, jsonPointers: true, loadSchema: loadSchema })
+
+function loadSchema(uri) {
+  //console.log(`loading schema: ${uri}`)
+  return new Promise((resolve, reject) => {
+    reject(`unable to resolve the schema ${uri}`)
+  })
+}
 
 function runValidateSchema(yamlSchemaFile) {
   try {
-    const json = yaml.readYAMLFile(yamlSchemaFile)
-    const valid = ajv.validate('http://json-schema.org/draft-07/schema#', json)
-    if (valid) {
-      console.log('ok')
-    }
-    else {
+    const schema = yaml.readYAMLFile(yamlSchemaFile)
+
+    // First, validate JSON Schema against JSON Schema Schema
+    const valid = ajv.validate('http://json-schema.org/draft-07/schema#', schema)
+    if (!valid) {
       // console.log(JSON.stringify(validate.errors, null, 2))
       console.log(ajv.errorsText(ajv.errors, { separator: '\n' }))
       process.exit(1)
     }
+
+    // Next, try to compile the Schema
+    ajv.compileAsync(schema)
+      .then((validate) => {
+        console.log('ok')
+      })
+      .catch((onrejected) => {
+        console.log(`Error: ${onrejected}`)
+        process.exit(1)
+      })
   }
   catch (e) {
     if (e.message) console.log(e.message)
