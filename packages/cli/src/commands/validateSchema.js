fc5105c890966b7318dabe7aa5a0b26954601758
zdenek@me.com <zdenek@me.com>
2018-02-25 16:26:41 +0100

    Validate directories

--- packages/cli/src/commands/validateSchema.js
+++ packages/cli/src/commands/validateSchema.js
@@ -1,31 +1,67 @@
 const path = require('path')
+const fs = require('fs')
 const schema = require('../lib/schema')
 const yaml = require('../lib/readYAML')
 
-function runValidateSchema(yamlSchemaFile) {
-  try {
-    // Parse YAML input
-    const schemaObject = yaml.readYAMLFile(yamlSchemaFile)
-    
-    // Get working directory for resolving local remote schemas
-    const cd = path.dirname(yamlSchemaFile)
-    
-    // Validate schema including references
-    schema.validateSchema(schemaObject, cd)
-      .then(() => {
-        console.log('ok.')
-      })
-      .catch((onrejected) => {
-        console.error(`Error: ${onrejected.message}`)
-        // console.error(onrejected)
-        process.exit(1)
-      })
+function isDirectory(path) {
+  return fs.statSync(path).isDirectory()
+}
+
+function readDirectory(dir) {
+  const content = fs.readdirSync(dir)
+  let files = []
+  content.forEach((file) => {
+    const cur = path.join(dir, file)
+    if (isDirectory(cur)) {
+      files = files.concat(readDirectory(cur))
+    }
+    else {
+      const ext = path.extname(file)
+      if (ext === '.yaml' || ext === '.yml') {
+        files.push(cur)
+      }
+    }
+  })
+  return files
+}
+
+function runValidateSchema(inputPath) {
+
+  let inputFiles = []
+  if (isDirectory(inputPath)) {
+    inputFiles = readDirectory(inputPath)
   }
-  catch (e) {
-    if (e.message) console.error(e.message)
-    else console.error(e)
-    process.exit(1)
+  else {
+    inputFiles.push(inputPath)
   }
+
+  inputFiles.forEach((file) => {
+    try {
+      // Parse YAML input
+      const schemaObject = yaml.readYAMLFile(file)
+
+      // Get working directory for resolving local remote schemas
+      const cd = path.dirname(file)
+
+      // Validate schema including references
+      schema.validateSchema(schemaObject, cd)
+        .then(() => {
+          console.log(`Passed: ${file}`)
+        })
+        .catch((onrejected) => {
+          console.error(`Error: ${onrejected.message}`)
+          // console.error(onrejected)
+          process.exit(1)
+        })
+    }
+    catch (e) {
+      console.error(`Error:\nin '${file}':`)
+      if (e.message) console.error(e.message)
+      else console.error(e)
+      process.exit(1)
+    }
+
+  })
 }
 
 module.exports = runValidateSchema
