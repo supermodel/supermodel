fd4bfb2f8905b638c3cf7f479a9a902c8cfc9b15
David Schovanec <schovanec@schovi.cz>
2018-04-23 16:31:35 +0200

    Initial auth0 authentication

---
+++ packages/cli/src/commands/signin.js
@@ -0,0 +1,30 @@
+const auth0 = require('auth0-js')
+
+const auth = new auth0.Authentication({
+  domain:       'goodapi.auth0.com',
+  clientID:     'TCJ1lTepFTDAfxe1QjPB70vgVKqqwpCl'
+});
+
+/**
+ * Runs signin command. Authenticate user against Auth0 and store necessary data ... TODO: which one?
+ *
+ * @param {string} username
+ * @param {string} password
+ */
+function signin(username, password) {
+  auth.login({
+    realm: 'Username-Password-Authentication',
+    username,
+    password,
+  }, function(error, response) {
+    if(error) {
+      console.error(`Sign in failed: ${error.description}`)
+      process.exit(1)
+    }
+
+    console.log('Sign in succesfully')
+    process.exit(0)
+  });
+}
+
+module.exports = signin
