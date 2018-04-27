07f2299bc87b72df85f5906e8c3067e070a814b3
David Schovanec <schovanec@schovi.cz>
2018-04-27 19:01:01 +0200

    Remove publishr and use .env.production as default

--- packages/cli/src/dotenv.js
+++ packages/cli/src/dotenv.js
@@ -5,7 +5,9 @@ function loadDotenv() {
   const file = path.resolve(__dirname, '../.env')
   const result = dotenv.config({ path: file })
   if (result.error) {
-    console.warn('Running development mode. Copy `.env.development` into `.env` and change variables as needed (\'cp .env.development .env\').')
+    const productionFile = path.resolve(__dirname, '../.env.production')
+    dotenv.config({ path: productionFile })
+    // console.warn('Running development mode. Copy `.env.development` into `.env` and change variables as needed (\'cp .env.development .env\').')
   }
 }
 
