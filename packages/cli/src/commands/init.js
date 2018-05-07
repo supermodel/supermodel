c73310f2902bba67de9b597a691b1bf261afa2ba
David Schovanec <schovanec@schovi.cz>
2018-05-07 20:33:10 +0200

    Move username requirement from lib/init into command/init

--- packages/cli/src/commands/init.js
+++ packages/cli/src/commands/init.js
@@ -1,4 +1,6 @@
+const path = require('path')
 const init = require('../lib/init')
+const cache = require('../cache')
 
 /**
  * Runs init command, creating a supermodel directory and initializing it with config
@@ -7,6 +9,13 @@ const init = require('../lib/init')
  */
 function runInit(layerPath) {
   try {
+    // Verify the user is logged in
+    const user = cache.get('user')
+    if (!user || ! user.username ) {
+      throw new Error(`Please login first using 'supermodel login'.`)
+    }
+
+    layerPath = path.join(user.username, layerPath)
     const createdDirectoryPath = init(layerPath)
 
     console.log(
@@ -15,7 +24,7 @@ function runInit(layerPath) {
 Use \'cd ${createdDirectoryPath}\' and \'supermodel model create <name>\' to create the first model.
 `)
   }
-  catch(e) { 
+  catch(e) {
     console.error(e)
     process.exit(1)
   }
--- packages/cli/src/lib/init.js
+++ packages/cli/src/lib/init.js
@@ -1,43 +1,42 @@
 const fs = require('fs')
 const path = require('path')
-const jsYaml = require('js-yaml')
-const cache = require('../cache')
 const fsUtils = require('./fsUtils')
 const supermodelConfig = require('./supermodelConfig')
 
 /**
  * Crates and initializes the supermodel directory
  *  optionally it also creates the directory structure to a layer
- * 
+ *
  * @param {string} layerPath Optional path to a layer to be created
+ * @param {Object} [config] Optional config to be stored in .super file
  * @returns {string} Path to the newly created directory
  * @throws {Error} In the case of failure an error is thrown
- * 
+ *
  */
 
- function init(layerPath) {
-  // Verify the user is logged in
-  const user = cache.get('user')
-  if (!user || ! user.username ) {
-    throw new Error(`Please login first using 'supermodel login'.`)
-  }
-
-  // Find  the relative 
-  let relativeDir = path.join(supermodelConfig.SUPERMODEL_DIR_NAME, user.username)
-  if (layerPath) {
-    relativeDir = path.join(relativeDir, layerPath)
-  }
+ function init(layerPath, config = null) {
+  // Find  the relative
+  let relativeDir = path.join(supermodelConfig.SUPERMODEL_DIR_NAME, layerPath)
 
   // Supermodel directory
   const supermodelDir = path.join(process.cwd(), relativeDir)
   fsUtils.mkdirpSync(supermodelDir)
 
   // Supermodel directory config file
-  const supermodelConfigFile = path.join(process.cwd(), 
+  const supermodelConfigFile = path.join(
+    process.cwd(),
     supermodelConfig.SUPERMODEL_DIR_NAME,
-    supermodelConfig.SUPERMODEL_CONFIG_FILENAME)
-  fs.closeSync(fs.openSync(supermodelConfigFile, 'a'))
-  // fs.writeFileSync(supermodelConfigFile, "")
+    supermodelConfig.SUPERMODEL_CONFIG_FILENAME
+  )
+
+  const configFileDescriptor = fs.openSync(supermodelConfigFile, 'w')
+
+  if (config !== null) {
+    const serializedConfig = JSON.stringify(config, null, 2)
+    fs.writeSync(configFileDescriptor, serializedConfig)
+  }
+
+  fs.closeSync(configFileDescriptor)
 
   return relativeDir
  }
