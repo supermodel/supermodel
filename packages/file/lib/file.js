184ad68d0e169be7d1a8aa63bc4171ce22101124
zdenek@me.com <zdenek@me.com>
2018-05-11 19:47:50 +0200

    Origin

---
+++ packages/file/lib/file.js
@@ -0,0 +1,8 @@
+const fileSchemaLoader = require('./fileSchemaLoader')
+const yamlFile = require('./yamlFile')
+const yamlModel = require('./yamlModel')
+
+module.exports = {
+  fileSchemaLoader,
+  yamlFile
+}
---
+++ packages/file/lib/yamlFile.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+
+/**
+ * Read YAML file
+ *
+ * @param {string} path Path to YAML file
+ * @return {object} Parsed YAML as a JS object
+ */
+function readYAMLFile(path) {
+  let buffer
+  try {
+    buffer = fs.readFileSync(path, 'utf8')
+  }
+  catch (e) {
+    if (e.code && e.code === 'ENOENT') {
+      const reason = `Error: no such file or directory; ${e.syscall} ${e.path}`
+      throw new Error(reason)
+    }
+    throw (e)
+  }
+
+  return readYAML(buffer)
+}
+
+/**
+ * Convert YAML file to JSON
+ *
+ * @param {string} path Path to the file
+ * @param {number} indentation Number of spaces to use for indentation
+ * @return {string} JSON string buffer
+ */
+function convertYAMLFileToJSON(path, indentation = 0) {
+  const buffer = readYAMLFile(path)
+  return convertYAMLToJSON(buffer, indentation)
+}
+
+module.exports = {
+  readYAMLFile,
+  convertYAMLFileToJSON
+}
