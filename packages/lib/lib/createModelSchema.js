d1946d54d390ef9a62b9959b1642f35b63751536
David Schovanec <schovanec@schovi.cz>
2018-05-11 20:23:28 +0200

    Add functions from CLI

---
+++ packages/lib/lib/createModelSchema.js
@@ -0,0 +1,20 @@
+function createModelSchema(basename, modelId) {
+  return `$id: ${modelId}
+$schema: http://json-schema.org/draft-07/schema#
+
+title: ${basename}
+description: ${basename} model description
+type: object  # Change to the desired model type (http://json-schema.org/latest/json-schema-validation.html#rfc.section.6.1.1)
+
+# Add your model properties here:
+#
+# properties:
+#   modelProperty:
+#     type: string
+#
+#   anotherProperty:
+#     $ref: AnotherModel  # Reference to another model in the same directory
+`
+}
+
+module.exports = createModelSchema
