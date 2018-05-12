648a8bd7ac2d69d6e6c3a86129d6ad7cee1b4fbb
zdenek@me.com <zdenek@me.com>
2018-05-12 14:34:16 +0200

    Require URL

--- packages/lib/src/convertToOAS2.js
+++ packages/lib/src/convertToOAS2.js
@@ -1,3 +1,5 @@
+const { URL } = require('url') // TODO: verify this works on browser
+
 const supportedKeys = ['title', 'multipleOf', 'maximum', 'exclusiveMaximum', 'minimum', 'exclusiveMinimum', 'maxLength', 'minLength', 'pattern', 'maxItems', 'minItems', 'uniqueItems', 'maxProperties', 'minProperties', 'required', 'enum', 'description', 'format', 'default']
 const schemaObjectArrayKeys = ['allOf' /*, 'anyOf', 'oneOf'*/]
 const schemaObjectKeys = [/*'not'*/, 'additionalProperties']
