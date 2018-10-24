6f8efbf4ad489514d7f298559889a339f2ed0b9f
David Schovanec <david@schovi.cz>
2018-10-24 14:38:23 +0200

    validate command allow to use local schema

--- packages/cli/src/commands/validate/validate.js
+++ packages/cli/src/commands/validate/validate.js
@@ -1,8 +1,11 @@
+const fs = require('fs')
+const path = require('path')
 const { URL } = require('url')
 const { validateData, validateMetaSchema, resolveSchema } = require('@supermodel/lib')
 const fetch = require('node-fetch')
 const readData = require('../../lib/readData')
 const remoteSchemaLoader = require('../../lib/remoteSchemaLoader')
+
 
 // For file operations
 // const fs = require('fs')
@@ -29,6 +32,20 @@ async function runValidate (dataFile, modelSchema) {
   }
 }
 
+function resolveFile(filePath) {
+  if (filePath.startsWith('~')) {
+    filePath = filePath.replace('~', process.env['HOME'])
+  } else {
+    filePath = path.resolve(filePath)
+  }
+
+  if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+    return filePath
+  }
+
+  return null
+}
+
 async function loadData(dataFile) {
   try {
     return await readData(dataFile)
@@ -41,24 +58,31 @@ async function loadData(dataFile) {
 
 async function loadSchema(modelSchema) {
   let schema
-  const url = new URL(modelSchema, process.env['SUPERMODEL_URL'])
 
-  try {
-    const response = await fetch(url.toString(), {
-      headers: {
-        'Accept': 'application/schema+json'
+  const file = resolveFile(modelSchema)
+
+  if (file) {
+    schema = await readData(file)
+  } else {
+    const url = new URL(modelSchema, process.env['SUPERMODEL_URL'])
+
+    try {
+      const response = await fetch(url.toString(), {
+        headers: {
+          'Accept': 'application/schema+json'
+        }
+      })
+
+      if (!response.ok) {
+        throw new Error(response.statusText)
       }
-    })
 
-    if (!response.ok) {
-      throw new Error(response.statusText)
+      schema = await response.json()
+    } catch(e) {
+      console.error('--> Failed load model:')
+      console.error(e.message)
+      process.exit(1)
     }
-
-    schema = await response.json()
-  } catch(e) {
-    console.error('--> Failed load model:')
-    console.error(e.message)
-    process.exit(1)
   }
 
   try {
