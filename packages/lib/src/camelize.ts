fd11809ff594e8571bc0d1a1f2a847090c54fb9d
David Schovanec <david@schovi.cz>
2019-03-06 21:14:25 +0100

    Fix camelize^2

--- packages/lib/src/camelize.ts
+++ packages/lib/src/camelize.ts
@@ -1,7 +1,9 @@
 import * as casex from 'casex';
 
+// Preserve casing of first word
 function camelize(str: string) {
-  return casex(str, 'SaSe');
+  str = str.trim();
+  return `${str[0]}${casex(str, 'caSe').slice(1)}`;
 }
 
 export default camelize;
--- packages/lib/test/camelize.test.ts
+++ packages/lib/test/camelize.test.ts
@@ -3,5 +3,7 @@ import camelize from '../src/camelize';
 describe('camelize', () => {
   test('converts to camelCase', () => {
     expect(camelize('some text')).toEqual('someText');
+    expect(camelize('Some text')).toEqual('SomeText');
+    expect(camelize('Some Text')).toEqual('SomeText');
   });
 });
