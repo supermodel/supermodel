9abae76a6e883fcdbf28faa0ef28e06eab6f9b78
David Schovanec <david@schovi.cz>
2019-03-06 21:57:00 +0100

    Fix publish

--- packages/lib/src/camelize.ts
+++ packages/lib/src/camelize.ts
@@ -1,6 +1,6 @@
 import * as casex from 'casex';
 
-// Preserve casing of first word
+// Preserve casing of first word and rest convert to CamelCase
 function camelize(str: string) {
   str = str.trim();
   return `${str[0]}${casex(str, 'caSe').slice(1)}`;
